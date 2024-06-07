416158b26ce1a88d5f2fbdb489b2ac425a0174e0
ppate343 <ppate343@uwo.ca>
2024-06-07 15:04:07 -0400

    cashly editor integration

---
+++ lender calculator/priv-calculator.js
@@ -0,0 +1,33 @@
+// inset all te priv lenders json object 
+const priv_lenders = {}; 
+
+//convert json object to js object
+
+
+
+// these will be the custom fields in cashly 
+const residential_type = 'First'; 
+const ltv = 0.80; 
+const fico = 550; 
+
+const options = [];
+
+
+
+
+// filter through the js object with specified values 
+function calculate(res_type, ltv, fico, appraisal_val) {
+
+
+    // filter json 
+
+    //calling calculate mortgage 
+      calculate_mortgage(appraisal_val); 
+
+}
+
+
+// function to calculate mortgage amount given appraisal + recommendations
+function calculate_mortgage(appr) {
+
+}
